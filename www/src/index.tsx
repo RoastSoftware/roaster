23052b06b14f6711ead4edf55981c3351dc6bbf0
Philip Hjortsberg <philip@hjortsberg.me>
2018-11-13 16:50:26 +0100

    www: removed react

--- www/src/index.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-
-import { Example } from "./components/Example";
-
-ReactDOM.render(
-    <Example compiler="TypeScript" framework="React" />,
-    document.getElementById("example")
-);
