ea8522662eaf8b478c7dd3eb45d5e0e1f9b0b149
William Wennerström <william@willeponken.me>
2018-11-28 10:46:06 +0100

    www/src/views: fix overflow problem for accessibility helpers

--- www/src/views/base.ts
+++ www/src/views/base.ts
@@ -13,6 +13,21 @@ margin: 0;\
 flex: 2;\
 `;
 
+// Instead of only using a { display: none; }, the element is positioned
+// off-screen, this allows screen readers to still see the content.
+// For more information, see:
+// https://developer.paciellogroup.com/blog/2012/05/html5-accessibility-chops-hidden-and-aria-hidden/
+const hideMonacoAriaContainer = `\
+clip-path: inset(100%);\
+clip: rect(1px 1px 1px 1px); /* IE 6/7 */\
+clip: rect(1px, 1px, 1px, 1px);\
+height: 1px;\
+overflow: hidden;\
+position: absolute;\
+white-space: nowrap; /* Added line */\
+width: 1px;\
+`;
+
 export default class Base implements ClassComponent {
   view(vnode: CVnode) {
     return [
@@ -20,6 +35,7 @@ export default class Base implements ClassComponent {
           ξ(nav),
           ξ('.ui.main.inverted', {style: mainContentContainer}, vnode.children),
       ),
+      ξ('style', '.monaco-aria-container {' + hideMonacoAriaContainer + '}'),
     ];
   }
 };
