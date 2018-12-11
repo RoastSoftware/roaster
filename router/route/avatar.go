babe04ebfddc62df174526da0de153229f645af7
William Wennerström <william@willeponken.me>
2018-12-11 15:20:59 +0100

    refactor+router: move controller to router and better error handling (#140)
    * refactor+router: move ctrl to router & initial better error handling
    * router/route: add causerr for all handlers
    * go.mod: update to latest version for causerr (removes newlines)

---
+++ router/route/avatar.go
@@ -0,0 +1,112 @@
+// Package route implements the avatar API endpoint.
+package route
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"mime"
+	"mime/multipart"
+	"net/http"
+	"strings"
+
+	"github.com/LuleaUniversityOfTechnology/2018-project-roaster/middleware"
+	"github.com/LuleaUniversityOfTechnology/2018-project-roaster/model"
+	"github.com/LuleaUniversityOfTechnology/2018-project-roaster/session"
+	"github.com/gorilla/mux"
+	"github.com/lib/pq"
+	"github.com/willeponken/causerr"
+)
+
+func createAvatar(w http.ResponseWriter, r *http.Request) (int, error) {
+	var a model.Avatar
+
+	s, err := session.Get(r, "roaster_auth")
+	if err != nil {
+		return http.StatusUnauthorized, causerr.New(
+			nil,
+			"Missing or unreadable cookie sent")
+	}
+
+	username, ok := s.Values["username"].(string)
+	if !ok || username == "" {
+		return http.StatusUnauthorized,
+			causerr.New(nil, "Unable to authenticate user")
+	}
+	mediaType, params, err := mime.ParseMediaType(r.Header.Get("Content-Type"))
+	if err != nil {
+		return http.StatusBadRequest,
+			causerr.New(err, "Missing Content-Type header in request")
+	}
+
+	if strings.HasPrefix(mediaType, "multipart/") {
+		mr := multipart.NewReader(r.Body, params["boundary"])
+		for {
+			p, err := mr.NextPart()
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				return http.StatusBadRequest,
+					causerr.New(err, "Unable to read data in request")
+			}
+			a.Avatar, err = ioutil.ReadAll(p)
+			if err != nil {
+				return http.StatusBadRequest,
+					causerr.New(err, "Unable to read data in request")
+			}
+			a.Username = username
+			if len(a.Avatar) >= 10*1000*1000 {
+				return http.StatusRequestEntityTooLarge,
+					causerr.New(
+						errors.New("too large file (greater than 10MB)"),
+						"Avatar picture must be less or equal to 10 MB")
+			}
+			err = model.PutAvatar(a)
+			if err != nil {
+				return http.StatusInternalServerError,
+					causerr.New(err, "")
+			}
+		}
+	} else {
+		return http.StatusBadRequest,
+			causerr.New(
+				errors.New("received wrong Content-Type, expected multipart/*"),
+				"Invalid Content-Type, expected multipart/*")
+	}
+
+	return http.StatusOK, nil
+}
+
+func retrieveAvatar(w http.ResponseWriter, r *http.Request) (int, error) {
+	var u string
+
+	vars := mux.Vars(r)
+	u = vars["username"]
+	avatar, err := model.GetAvatar(u)
+	if err, ok := err.(*pq.Error); ok {
+		if err.Code.Name() == "foreign_key_violation" {
+			return http.StatusNotFound,
+				causerr.New(nil, fmt.Sprintf("No avatar found for '%s'", u))
+		}
+		return http.StatusInternalServerError, causerr.New(err, "")
+	}
+	_, err = w.Write(avatar.Avatar)
+	if err != nil {
+		return http.StatusInternalServerError, causerr.New(err, "")
+	}
+
+	return http.StatusOK, nil
+}
+
+// Avatar adds the handlers for the Avatar
+func Avatar(s *mux.Router) {
+	s.Use(middleware.EnforceContentType("multipart/form-data"))
+
+	// Create avatar for user [PUT].
+	s.Handle("", handler(createAvatar)).Methods(http.MethodPut)
+
+	// Retrieve avatar for user [GET].
+	s.Handle("", handler(retrieveAvatar)).Methods(http.MethodGet)
+}
