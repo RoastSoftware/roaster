e1ff56947db55af6212cbfe457da7e220a8e2871
William Wennerström <william@willeponken.me>
2018-12-16 14:48:58 +0100

    model/statistics: continue on statistics, untested functions

--- model/statistics.go
+++ model/statistics.go
@@ -2,7 +2,23 @@
 package model
 
 import (
+	"fmt"
 	"time"
+)
+
+const (
+	// SecondResolution represents a single second.
+	SecondResolution = time.Second
+	// MinuteResolution is exactly 60 seconds.
+	MinuteResolution = time.Minute
+	// HourResolution is exactly 60 minutes.
+	HourResolution = time.Hour
+	// DayResolution is exactly 24 hours.
+	DayResolution = time.Hour * 24
+	// MonthResolution is exactly 30 days.
+	MonthResolution = DayResolution * 30
+	// YearResolution is exactly 365 days.
+	YearResolution = DayResolution * 365
 )
 
 // LanguageDatapoint represents a datapoint for language statistics.
@@ -18,6 +34,16 @@ type LanguageDatapoint struct {
 type LanguageStatistics struct {
 	Datapoints []LanguageDatapoint `json:"items"`
 }
+
+// RoastCountDatapoint represents a single datapoint with a timestamp and the
+// count of Roasts during that timestamp.
+type RoastCountDatapoint struct {
+	Timestamp time.Time `json:"timestamp"`
+	Count     uint64    `json:"count"`
+}
+
+// RoastCountTimeseries holds a slice of RoastCountDatapoints.
+type RoastCountTimeseries []RoastCountDatapoint
 
 // GetGlobalLinesOfCodeForLanguage returns the number of lines of code for all
 // users for a specific language.
@@ -68,19 +94,120 @@ func GetUserScore(username string) (score uint64, err error) {
 	return
 }
 
-/* TODO: Time series of Roasts/(day/hour/w/e)
-select "create_time"::date, count(distinct "id")
-from "roaster"."roast"
-group by "create_time"::date;
+// GetGlobalRoastCountTimeseries returns a timeseries between a start and end
+// timestamp, the resolution parameters should be any of:
+//  * SecondResolution,
+//  * MinuteResolution,
+//  * HourResolution,
+//  * DayResolution   - exactly 24 hours,
+//  * MonthResolution - exactly 30 days,
+//  * YearResolution  - exactly 365 days.
+//
+// If the resolution doesn't match any of the supported resolutions above, it'll
+// default to the YearResolution.
+//
+// The resolutions are exact and does not take into account leap seconds etc.
+func GetGlobalRoastCountTimeseries(start, end time.Time, resolution time.Duration) (
+	timeseries RoastCountTimeseries, err error) {
 
--- Or...
+	return getRoastCountTimeseries(start, end, resolution, "")
+}
 
-select
-  date_trunc('minute', create_time), -- or hour, day, week, month, year
-  count(1)
-from roaster.roast
-group by 1
-*/
+// GetUserRoastCountTimeseries returns a timeseries between a start and end
+// timestamp for the provided username, the resolution parameters should be any
+// of:
+//  * SecondResolution,
+//  * MinuteResolution,
+//  * HourResolution,
+//  * DayResolution   - exactly 24 hours,
+//  * MonthResolution - exactly 30 days,
+//  * YearResolution  - exactly 365 days.
+//
+// If the resolution doesn't match any of the supported resolutions above, it'll
+// default to the YearResolution.
+//
+// The resolutions are exact and does not take into account leap seconds etc.
+func GetUserRoastCountTimeseries(start, end time.Time, resolution time.Duration, username string) (
+	timeseries RoastCountTimeseries, err error) {
+
+	return getRoastCountTimeseries(start, end, resolution, username)
+}
+
+// getRoastCountTimeseries returns a timeseries of number of Roasts per time
+// unit. See: GetGlobalRoastCountTimeseries or GetUserRoastCountTimeseries.
+func getRoastCountTimeseries(start, end time.Time, resolution time.Duration, username string) (
+	timeseries RoastCountTimeseries, err error) {
+
+	sqlInterval, sqlResolution := getSQLResolution(resolution)
+	sqlStart := formatSQLTime(start)
+	sqlEnd := formatSQLTime(end)
+
+	rows, err := database.Query(`
+		WITH "time_series" AS (
+			SELECT generate_series(
+				$1::timestamp, -- Start point date with time.
+				$2::timestamp, -- End point date with time.
+				$3::interval   -- Time series resolution.
+			) AS "datapoint"
+		)
+
+		SELECT
+			"time_series"."datapoint",
+			count(distinct r."id")
+		FROM "time_series"
+		LEFT JOIN "roaster"."roast" AS r
+			-- Truncate and compare per resolution.
+			ON date_trunc($4, r."create_time") = "time_series"."datapoint"
+			AND COALESCE(TRIM($4), '')='' OR LOWER(username)=LOWER(TRIM($4))
+		GROUP BY 1
+	`, sqlStart, sqlEnd, sqlInterval, sqlResolution, username)
+	if err != nil {
+		return
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		datapoint := RoastCountDatapoint{}
+
+		err = rows.Scan(&datapoint.Timestamp, &datapoint.Count)
+		if err != nil {
+			return
+		}
+
+		timeseries = append(timeseries, datapoint)
+	}
+
+	return
+}
+
+// getSQLResolution returns the SQL interval and resolution for the provided
+// resolution.
+func getSQLResolution(resolution time.Duration) (sqlInterval, sqlResolution string) {
+	var resIdentifier string
+	switch resolution {
+	case SecondResolution:
+		resIdentifier = "second"
+	case MinuteResolution:
+		resIdentifier = "minute"
+	case HourResolution:
+		resIdentifier = "hour"
+	case DayResolution:
+		resIdentifier = "day"
+	case YearResolution:
+	default:
+		resIdentifier = "year"
+	}
+
+	sqlInterval = fmt.Sprintf("%d %s", 1, resIdentifier)
+	sqlResolution = resIdentifier
+
+	return
+}
+
+// formatSQLTime returns the time formatted as RFC3339 w/ decimals.
+func formatSQLTime(t time.Time) string {
+	return t.Format("2006-01-02T15:04:05.000000Z")
+}
 
 func getGlobalLinesOfCode(language string) (lines uint64, err error) {
 	err = database.QueryRow(`
