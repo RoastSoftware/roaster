0523f3d7130dfa6ae9031e158127559800822a78
William Wennerström <william@willeponken.me>
2018-12-20 14:39:47 +0100

    www: add global and user feed (#159)

--- www/src/views/statistics.ts
+++ www/src/views/statistics.ts
@@ -6,7 +6,14 @@ import Model from '../models/statistics';
 export default class Statistics implements ξ.ClassComponent {
   view(vnode: ξ.CVnode) {
     return ξ(base,
-        ξ('.ui.main.text.container[style=margin-top: 2em;]',
+        ξ('.ui.main.text.container[style=margin-top: 1em;]',
+            ξ('h1.ui.header',
+                ξ('i.chart.bar.icon'),
+                ξ('.content', 'STATISTICS',
+                    ξ('.sub.header', 'All em\' statistics we\'ve collected.')),
+            ),
+            ξ('.ui.divider')),
+        ξ('.ui.main.text.container',
             ξ('p', 'Let there be GRAPHS! '),
             ξ('p', 'later...'),
             ξ('canvas#chart-area', {
