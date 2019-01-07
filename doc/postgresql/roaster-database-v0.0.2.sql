RoastSoftware/roaster

commit ad2ffc6e0815f2e182959ca4fddb5d969ef74933

cmd+doc+model+router+www: more strict columns in pg using domains (fixes: #202) (#206)
* www: fix user page onupdate and update Mithril with one minor
* cmd+doc+model+router+www: more strict columns in pg using domains
* doc+model+www: I give up, control/unsafe characters are banned
 + The Go router is unable to handle some escaped control/unsafe
 characters, and double encoding some specific characters feels really
 bad. So what the hell.
* doc+model+www: support as many characters as possible
* www: fix wrong uri for followees

doc/postgresql/roaster-database-v0.0.2.sql
@@ -1,27 +1,43 @@
 set search_path to roaster;
 
+do $$
+begin
+  -- Almost everything with an @ is a valid e-mail, so what the hell.
+  create domain email as text check (value ~ '@' and
+				     char_length(value) < 255 and
+				     char_length(value) > 2); --
+
+  create domain username as text check (value !~* E'[\\s:;,+$\\/\\\\?!*\'()@=&#]' and
+					char_length(value) > 0 and
+					char_length(value) <= 30); --
+
+  create domain fullname as text check(char_length(value) < 255); --
+
+  create domain code as text check(char_length(value) <= 500000); --
+
+  create domain score as integer check (value >= 0); --
+
+  exception when others then
+    raise notice 'domains already exists, skipping...'; --
+end
+$$;
+
 create table if not exists "user"
 (
-  username text  not null
+  username username not null
     constraint user_pkey
-    primary key
-    constraint username_chk
-    check (char_length(username) <= 30),
+    primary key,
   hash     bytea not null,
   create_time	timestamp with time zone not null,
-  fullname text
-    constraint fullname_chk
-    check (char_length(fullname) < 255),
-  email    text  not null unique
-    constraint email_chk
-    check (char_length(email) < 255)
+  fullname fullname,
+  email    email not null unique
 );
 
 create unique index if not exists username_user_idx on "user" (lower(username));
 
 create table if not exists user_followees
 (
-    username text not null,
+    username username not null,
     create_time timestamp with time zone not null,
     followee text not null,
     constraint followee_relation_uq unique (username, followee),
@@ -43,17 +59,13 @@ create table if not exists "roast"
   id       	serial        	not null
     constraint roast_pk
     primary key,
-  code     	text          	not null
-    constraint code_chk
-    check (char_length(code) <= 500000),
-  username 	text          	not null
+  code     	code 		not null,
+  username 	username	not null
     constraint user_fk
     references "user" (username)
     on update cascade
     on delete cascade,
-  score    	integer 	not null
-    constraint score_chk
-    check (score >= 0),
+  score    	score 		not null,
   language 	text          	not null,
   create_time	timestamp	with time zone not null
 );
@@ -121,7 +133,7 @@ create table if not exists "roast_has_warnings"
 create table if not exists avatar
 (
   avatar bytea not null,
-  username text not null,
+  username username not null,
   constraint username_uq unique (username),
   constraint username_fk foreign key (username)
     references "user" (username) match simple

model/forwardengineer/schema.go
@@ -1,21 +1,22 @@
-// Package forwardengineer was generated automatically by inlinesql at 2019-01-05 22:59:18.6727047 +0100 CET m=+0.007774301.
+// Package forwardengineer was generated automatically by inlinesql at 2019-01-07 14:50:06.856777351 +0100 CET m=+0.001907264.
 package forwardengineer
 
 // GetQueries returns a pre-parsed slice of SQL queries.
 func GetQueries() []string {
 	return []string{
 		"set search_path to roaster",
-		"create table if not exists \"user\" ( username text not null constraint user_pkey primary key constraint username_chk check (char_length(username) <= 30), hash bytea not null, create_time timestamp with time zone not null, fullname text constraint fullname_chk check (char_length(fullname) < 255), email text not null unique constraint email_chk check (char_length(email) < 255) )",
+		"do $$ begin create domain email as text check (value ~ '@' and char_length(value) < 255 and char_length(value) > 2); create domain username as text check (value !~* E'[\\\\s:;,+$\\\\/\\\\\\\\?!*\\'()@=&#]' and char_length(value) > 0 and char_length(value) <= 30); create domain fullname as text check(char_length(value) < 255); create domain code as text check(char_length(value) <= 500000); create domain score as integer check (value >= 0); exception when others then raise notice 'domains already exists, skipping...'; end $$",
+		"create table if not exists \"user\" ( username username not null constraint user_pkey primary key, hash bytea not null, create_time timestamp with time zone not null, fullname fullname, email email not null unique )",
 		"create unique index if not exists username_user_idx on \"user\" (lower(username))",
-		"create table if not exists user_followees ( username text not null, create_time timestamp with time zone not null, followee text not null, constraint followee_relation_uq unique (username, followee), constraint username_fk foreign key (username) references \"user\" (username) match simple on update cascade on delete cascade, constraint followee_fk foreign key (username) references \"user\" (username) match simple on update cascade on delete cascade )",
+		"create table if not exists user_followees ( username username not null, create_time timestamp with time zone not null, followee text not null, constraint followee_relation_uq unique (username, followee), constraint username_fk foreign key (username) references \"user\" (username) match simple on update cascade on delete cascade, constraint followee_fk foreign key (username) references \"user\" (username) match simple on update cascade on delete cascade )",
 		"create index if not exists username_user_followees_idx on user_followees using btree (username)",
-		"create table if not exists \"roast\" ( id serial not null constraint roast_pk primary key, code text not null constraint code_chk check (char_length(code) <= 500000), username text not null constraint user_fk references \"user\" (username) on update cascade on delete cascade, score integer not null constraint score_chk check (score >= 0), language text not null, create_time timestamp with time zone not null )",
+		"create table if not exists \"roast\" ( id serial not null constraint roast_pk primary key, code code not null, username username not null constraint user_fk references \"user\" (username) on update cascade on delete cascade, score score not null, language text not null, create_time timestamp with time zone not null )",
 		"create table if not exists \"warning\" ( id uuid not null constraint warning_pk primary key, row integer not null, \"column\" integer not null, engine text not null, name text not null, description text not null )",
 		"create table if not exists \"error\" ( id uuid not null constraint error_pk primary key, row integer not null, \"column\" integer not null, engine text not null, name text not null, description text not null )",
 		"create table if not exists \"roast_statistics\" ( roast integer not null constraint roast_fk references roast (id) on update cascade on delete cascade, lines_of_code integer not null, number_of_errors integer not null, number_of_warnings integer not null )",
 		"create table if not exists \"roast_has_errors\" ( roast integer not null constraint roast_fk references roast (id) on update cascade on delete cascade, error uuid not null constraint error_fk references error (id) )",
 		"create table if not exists \"roast_has_warnings\" ( roast integer not null constraint roast_fk references roast (id) on update cascade on delete cascade, warning uuid not null constraint warning_fk references warning (id) )",
-		"create table if not exists avatar ( avatar bytea not null, username text not null, constraint username_uq unique (username), constraint username_fk foreign key (username) references \"user\" (username) match simple on update cascade on delete cascade )",
+		"create table if not exists avatar ( avatar bytea not null, username username not null, constraint username_uq unique (username), constraint username_fk foreign key (username) references \"user\" (username) match simple on update cascade on delete cascade )",
 		"create index if not exists username_avatar_idx on avatar using btree (username)",
 		"drop function if exists round_minutes(timestamp without time zone, integer)",
 		"drop function if exists round_minutes(timestamp without time zone, integer, text)",

www/src/views/user.ts
@@ -1,7 +1,7 @@
 import ξ from 'mithril';
 import Chart from 'chart.js';
 import base from './base';
-import Network from '../services/network';
+import Network, {encodeURL} from '../services/network';
 import {UserModel} from '../models/user';
 import {FeedList} from './feed';
 import {
@@ -64,7 +64,7 @@ export class UserProfileHeader implements ξ.ClassComponent {
     },
     ξ('h1.ui.header.left.floated[style=margin: 0;]',
         ξ('img.ui.circular.image', {
-          src: attrs.avatar || `/user/${attrs.username}/avatar`,
+          src: attrs.avatar || encodeURL('user', attrs.username, 'avatar'),
         }),
         attrs.loggedIn ?
         ξ('.content', 'MY PROFILE',
@@ -82,9 +82,9 @@ class UserLink implements ξ.ClassComponent {
     const username: string = attrs.username;
 
     return ξ('a.user', {
-      href: `/user/${username.toLowerCase()}`,
+      href: `/user/${encodeURIComponent(username)}`,
       oncreate: ξ.route.link,
-    }, username);
+    }, username.toUpperCase());
   };
 }
 
@@ -93,8 +93,8 @@ export class UserFolloweeList implements ξ.ClassComponent {
     followees: Array<Followee> = [];
 
     async getFolloweeList() {
-      return Network.request<Array<Followee>>('GET', '/user/' +
-            this.username + '/followees')
+      return Network.request<Array<Followee>>('GET',
+          ['user', this.username, 'followees'])
           .then((result: any) => {
             this.followees = result;
           });
@@ -119,12 +119,12 @@ export class UserFolloweeList implements ξ.ClassComponent {
               return ξ('.event',
                   ξ('.label',
                       ξ('img', {
-                        src: `/user/${i.username}/avatar`,
+                        src: encodeURL('user', this.username, 'avatar'),
                       }),
                   ),
                   ξ('.content',
                       ξ('.summary',
-                          ξ(UserLink, {'username': i.username.toUpperCase()}),
+                          ξ(UserLink, {'username': this.username}),
                           ξ('.date[style=float:right;]',
                               ξ('i.clock.outline.icon'),
                               'followed '
@@ -144,8 +144,8 @@ export class UserFollowerList implements ξ.ClassComponent {
     username: string;
 
     async getFollowerList(username: string) {
-      return Network.request<Array<Follower>>('GET', '/user/' +
-            this.username + '/followers')
+      return Network.request<Array<Follower>>('GET',
+          ['user', this.username, 'followers'])
           .then((result: any) => {
             this.followers = result;
           });
@@ -170,12 +170,12 @@ export class UserFollowerList implements ξ.ClassComponent {
               return ξ('.event',
                   ξ('.label',
                       ξ('img', {
-                        src: `/user/${i.username}/avatar`,
+                        src: encodeURL('user', this.username, 'avatar'),
                       }),
                   ),
                   ξ('.content',
                       ξ('.summary',
-                          ξ(UserLink, {'username': i.username.toUpperCase()}),
+                          ξ(UserLink, {'username': this.username}),
                           ξ('.date[style=float:right;]',
                               ξ('i.clock.outline.icon'),
                               'followed '
@@ -209,7 +209,8 @@ export class UserFeed implements ξ.ClassComponent {
 
   fetchFeed() {
     const username = this.username;
-    Network.request<Feed>('GET', `/feed?page=0&page-size=5&user=${username}`)
+    Network.request<Feed>('GET',
+        `/feed?page=0&page-size=5&user=${username}`)
         .then((feed: Feed) => {
           this.feed = feed;
         }).catch((error) => {
@@ -253,8 +254,8 @@ class UserProfile implements ξ.ClassComponent {
     };
 
     async friendCheck(username: string) {
-      return Network.request<Array<Followee>>('GET', '/user/' +
-            UserModel.getUsername() + '/followees')
+      return Network.request<Array<Followee>>('GET',
+          ['user', UserModel.getUsername(), 'followees'])
           .then((result) => {
             UserModel.followees = result;
             this.hasFriend(username);
@@ -262,14 +263,14 @@ class UserProfile implements ξ.ClassComponent {
     };
 
     async unFriend(username: string) {
-      Network.request('DELETE', '/user/' + username + '/followees')
+      Network.request('DELETE', ['user', username, 'followees'])
           .then(() => {
             this.friendCheck(username);
           });
     };
 
     async addFriend(username: string) {
-      Network.request('POST', '/user/' + username + '/followees', {
+      Network.request('POST', ['user', username, 'followees'], {
         'username': username,
       })
           .then(() => {
@@ -304,7 +305,7 @@ class UserProfile implements ξ.ClassComponent {
               ξ('.ui.row',
                   ξ('.column',
                       ξ('img.ui.image.rounded.medium#picture', {
-                        src: `/user/${username}/avatar`,
+                        src: encodeURL('user', username, 'avatar'),
                       },
                       'User profile picture.'),
                       ξ('h2',
@@ -405,12 +406,12 @@ export default class UserView implements ξ.ClassComponent {
     score: number = 0;
 
     fetchUser() {
-      Network.request<User>('GET', '/user/' + this.username)
+      Network.request<User>('GET', ['user', this.username])
           .then((user: User) => {
             this.user = user;
           });
 
-      Network.request<Object>('GET', '/user/' + this.username + '/score')
+      Network.request<Object>('GET', ['user', this.username, 'score'])
           .then(({score}) => {
             this.score = score;
           });

www/src/models/user.ts
@@ -41,11 +41,20 @@ export class UserModel {
     }
 
     static setUsername(user: string) {
+      // Disallow usernames with whitespaces and URL control/unsafe characters.
+      const re = new RegExp(/[\s:;,+$\/\\?!*\'()@=&#]/);
+
       UserModel.username = user;
+
       if (UserModel.username.length < 1) {
-        UserModel.usernameError = 'Username must be longer than 0 characters';
-      } else if (UserModel.username.length > 30) {
-        UserModel.usernameError = 'Username must be shorter than 30 characters';
+        UserModel.usernameError =
+          'Username must be longer than 0 characters';
+      } else if (UserModel.username.length >= 30) {
+        UserModel.usernameError =
+          'Username must not be longer than 30 characters';
+      } else if (re.test(UserModel.username)) {
+        UserModel.usernameError =
+          'Username must not contain spaces or any of :;,+$\/\\?!*\'()@=&#';
       } else {
         UserModel.usernameError = '';
       }
@@ -102,6 +111,9 @@ that is more than 1 googol^98 in entropy`;
     };
 
     static setEmail(user: string) {
+      // The database only cares that the e-mail contains an @ and is longer
+      // than 2 characters. But we're using this crazy RegExp here that _should_
+      // take care of _most_ cases.
       /* eslint max-len: ["error", { "ignoreRegExpLiterals": true }] */
       const re = new RegExp(/^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i);
 
@@ -127,12 +139,23 @@ that is more than 1 googol^98 in entropy`;
       return UserModel.emailError;
     };
 
+    static allErrors(): Array<string> {
+      const errors = [];
+
+      errors.push(UserModel.errorUsername());
+      errors.push(UserModel.errorPassword());
+      errors.push(UserModel.errorFullname());
+      errors.push(UserModel.errorEmail());
+
+      return errors.filter((e) => e != '');
+    };
+
     static validAll(): boolean {
       return (
         UserModel.validUsername() && UserModel.validEmail() &&
         UserModel.validFullname() && UserModel.validPassword()
       ) && (
-        (UserModel.getUsername() !='') && (UserModel.getEmail() != '') &&
+        (UserModel.getUsername() != '') && (UserModel.getEmail() != '') &&
         (UserModel.getFullname() != '') && (UserModel.getPassword() != ''));
     };
 
@@ -159,4 +182,3 @@ that is more than 1 googol^98 in entropy`;
       UserModel.followees = [];
     };
 };
-

cmd/inlinesql/main.go
@@ -23,7 +23,7 @@ func parseSQLFile(in []byte) []string {
 
 	// Split file into seperate queries.
 	var tmp []string
-	tmp = strings.Split(string(in), ";")
+	tmp = strings.Split(string(in), ";\n")
 
 	// Remove superflous spaces around each query.
 	for i := range tmp {

model/user.go
@@ -114,7 +114,7 @@ func PutUser(user User, password []byte) (err error) {
 		INSERT INTO "roaster"."user"
 		(username, email, fullname, create_time, hash)
 		VALUES
-		(TRIM($1), LOWER(TRIM($2)), $3, $4, $5)
+		($1, TRIM($2), TRIM($3), $4, $5)
 	`, user.Username, user.Email, user.Fullname, time.Now(), hash)
 
 	return

router/route/user.go
@@ -35,20 +35,31 @@ func createUser(w http.ResponseWriter, r *http.Request) (int, error) {
 			"Unable to decode request")
 	}
 
-	// TODO: Maybe add some kind of helper for empty fields?
 	if u.Username == "" || u.Email == "" || u.Password == "" {
 		return http.StatusBadRequest, causerr.New(
 			errors.New("missing field in request"),
 			"Missing field in request")
 	}
 
+	if len(u.Password) < 8 || len(u.Password) >= 4096 {
+		return http.StatusBadRequest, causerr.New(
+			fmt.Errorf("invalid password length (%d)", len(u.Password)),
+			"Invalid password")
+	}
+
 	err = model.PutUser(u.User, []byte(u.Password))
 	if err != nil {
 		if pgerr, ok := err.(*pq.Error); ok {
-			if pgerr.Constraint == "user_email_key" {
+			switch pgerr.Constraint {
+			case "username_check":
+				return http.StatusBadRequest, causerr.New(err, "Invalid username")
+			case "email_check":
+				return http.StatusBadRequest, causerr.New(err, "Invalid email address")
+			case "fullname_check":
+				return http.StatusBadRequest, causerr.New(err, "Invalid full name")
+			case "user_email_key":
 				return http.StatusConflict, causerr.New(err, "Email already in use")
-			}
-			if pgerr.Constraint == "user_pkey" || pgerr.Constraint == "username_user_idx" {
+			case "user_pkey":
 				return http.StatusConflict, causerr.New(err, "Username already in use")
 			}
 		}

www/src/services/network.ts
@@ -23,6 +23,18 @@ class APIError extends Error {
   }
 };
 
+export function encodeURL(...url: string): string {
+  let u = '';
+
+  if (url.length > 1) {
+    u = '/' + url.join('/');
+  } else {
+    u = url[0];
+  }
+
+  return encodeURI(u);
+};
+
 export default class Network {
   private static nextCSRFToken: string = '';
 
@@ -59,7 +71,7 @@ export default class Network {
 
   public static async request<T>(
       method: string,
-      url: string,
+      url: string | Array<string>,
       data?: any): Promise<T> {
     if (Network.nextCSRFToken == '') {
       await Network.initCSRFToken();
@@ -67,7 +79,7 @@ export default class Network {
 
     return ξ.request<T>({
       method: method,
-      url: url,
+      url: Array.isArray(url) ? encodeURL(...url) : encodeURL(url),
       data: data,
       headers: {[xCsrfToken]: Network.nextCSRFToken},
       extract: Network.extract,

commit 75715dd6592a0f6e2ba87397479d4d87998ae2ef

www/src/user: fix followee/follower regression since #206

doc/postgresql/roaster-database-v0.0.2.sql
@@ -39,7 +39,7 @@ create table if not exists user_followees
 (
     username username not null,
     create_time timestamp with time zone not null,
-    followee text not null,
+    followee username not null,
     constraint followee_relation_uq unique (username, followee),
     constraint username_fk foreign key (username)
         references "user" (username) match simple

model/forwardengineer/schema.go
@@ -1,4 +1,4 @@
-// Package forwardengineer was generated automatically by inlinesql at 2019-01-07 14:50:06.856777351 +0100 CET m=+0.001907264.
+// Package forwardengineer was generated automatically by inlinesql at 2019-01-07 17:11:26.371319814 +0100 CET m=+0.001358337.
 package forwardengineer
 
 // GetQueries returns a pre-parsed slice of SQL queries.
@@ -8,7 +8,7 @@ func GetQueries() []string {
 		"do $$ begin create domain email as text check (value ~ '@' and char_length(value) < 255 and char_length(value) > 2); create domain username as text check (value !~* E'[\\\\s:;,+$\\\\/\\\\\\\\?!*\\'()@=&#]' and char_length(value) > 0 and char_length(value) <= 30); create domain fullname as text check(char_length(value) < 255); create domain code as text check(char_length(value) <= 500000); create domain score as integer check (value >= 0); exception when others then raise notice 'domains already exists, skipping...'; end $$",
 		"create table if not exists \"user\" ( username username not null constraint user_pkey primary key, hash bytea not null, create_time timestamp with time zone not null, fullname fullname, email email not null unique )",
 		"create unique index if not exists username_user_idx on \"user\" (lower(username))",
-		"create table if not exists user_followees ( username username not null, create_time timestamp with time zone not null, followee text not null, constraint followee_relation_uq unique (username, followee), constraint username_fk foreign key (username) references \"user\" (username) match simple on update cascade on delete cascade, constraint followee_fk foreign key (username) references \"user\" (username) match simple on update cascade on delete cascade )",
+		"create table if not exists user_followees ( username username not null, create_time timestamp with time zone not null, followee username not null, constraint followee_relation_uq unique (username, followee), constraint username_fk foreign key (username) references \"user\" (username) match simple on update cascade on delete cascade, constraint followee_fk foreign key (username) references \"user\" (username) match simple on update cascade on delete cascade )",
 		"create index if not exists username_user_followees_idx on user_followees using btree (username)",
 		"create table if not exists \"roast\" ( id serial not null constraint roast_pk primary key, code code not null, username username not null constraint user_fk references \"user\" (username) on update cascade on delete cascade, score score not null, language text not null, create_time timestamp with time zone not null )",
 		"create table if not exists \"warning\" ( id uuid not null constraint warning_pk primary key, row integer not null, \"column\" integer not null, engine text not null, name text not null, description text not null )",

www/src/views/user.ts
@@ -119,12 +119,12 @@ export class UserFolloweeList implements ξ.ClassComponent {
               return ξ('.event',
                   ξ('.label',
                       ξ('img', {
-                        src: encodeURL('user', this.username, 'avatar'),
+                        src: encodeURL('user', i.username, 'avatar'),
                       }),
                   ),
                   ξ('.content',
                       ξ('.summary',
-                          ξ(UserLink, {'username': this.username}),
+                          ξ(UserLink, {'username': i.username}),
                           ξ('.date[style=float:right;]',
                               ξ('i.clock.outline.icon'),
                               'followed '
@@ -170,12 +170,12 @@ export class UserFollowerList implements ξ.ClassComponent {
               return ξ('.event',
                   ξ('.label',
                       ξ('img', {
-                        src: encodeURL('user', this.username, 'avatar'),
+                        src: encodeURL('user', i.username, 'avatar'),
                       }),
                   ),
                   ξ('.content',
                       ξ('.summary',
-                          ξ(UserLink, {'username': this.username}),
+                          ξ(UserLink, {'username': i.username}),
                           ξ('.date[style=float:right;]',
                               ξ('i.clock.outline.icon'),
                               'followed '