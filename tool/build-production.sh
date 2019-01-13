a756040188415ce14063c502f6cfd645524f86a0
William Wennerström <william@willeponken.me>
2019-01-13 14:27:25 +0100

    tool: switch to aglio for rendering REST docs

--- tool/build-production.sh
+++ tool/build-production.sh
@@ -15,9 +15,14 @@ docker run --rm -v "$PWD/www":/usr/src/roaster -w /usr/src/roaster node /bin/bas
 docker run --rm -v "$PWD":/usr/src/roaster -w /usr/src/roaster/cmd/roasterd golang:1.11 go build -o /usr/src/roaster/build/roasterd
 
 # Build API docs
-docker run --rm -v $PWD/doc/restapi:/doc quay.io/bukalapak/snowboard html \
-	-o index.html Roaster-REST-API.md && \
-	mv ./doc/restapi/index.html ./build/doc/restapi/
+git clone https://github.com/danielgtaylor/aglio.git /tmp/roaster-build-aglio
+pushd /tmp/roaster-build-aglio
+docker build -t aglio .
+popd
+docker run --rm -v $PWD/doc/restapi:/tmp -t aglio --theme-template triple \
+	-i /tmp/Roaster-REST-API.md -o /tmp/index.html \
+	&& mv -f ./doc/restapi/index.html ./build/doc/restapi/
+rm -rf /tmp/roaster-build-aglio
 
 # Current build folder content
 du -h -d1 build/*
